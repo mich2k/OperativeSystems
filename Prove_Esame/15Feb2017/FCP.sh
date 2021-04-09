1efb3c11e0b85ccd67d84481a4bfca1493042523
Michele <miky3848@gmail.com>
2021-04-09 12:08:19 +0200

    fix past exam + 9Feb2015

--- Prove_Esame/15Feb2017/FCP.sh
+++ Prove_Esame/15Feb2017/FCP.sh
@@ -2,13 +2,14 @@
 
 if [ ! $# -eq 2 ];then
     echo il numero di parametri deve essere 2, non $#
+    exit 1
 fi
 
 case $1 in
     /*);;
     *)
         echo errore, $1 non e\' una directory assoluta
-        exit 1;;
+        exit 2;;
 esac
 
 expr $2 + 0 > /dev/null 2>&1
@@ -16,11 +17,11 @@ if [ ! $? -eq 2 -a ! $? -eq 3 ]; then
     #echo $2 e\' numerico
     if [ ! $2 -ge 0 ]; then
         echo $2 non e\' strettamente positivo
-        exit 2
+        exit 3
     fi
 else
     echo $2 non numerico
-    exit 3
+    exit 4
 fi
 
 
@@ -33,4 +34,5 @@ if [ -d $1 -a -x $1 ];then
     FCR.sh $1 $2
 else
     echo $1 non dir o non attraversabile
+    exit 5
 fi
